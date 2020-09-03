20930458a391daac42f8469ec1adf54e552c7546
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2020-09-03 22:13:25 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'unittest',
-        'datetime',
         'parameterized',
         'selenium',
         'pyvirtualdisplay',
