d371ae640e0bf522ca940f7c2442e5f86ba66743
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2020-09-16 08:59:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,9 @@ xpath_login_id = (
 )
 xpath_units = '//*[@id="main-menu-nav"]/ul/li[2]/a'
 
+ERP_url = 'https://comap-control.operations.dynamics.com/'
+
 status_file = "status.yaml"
 
 test_enabled = True
+send_to_teams = True
